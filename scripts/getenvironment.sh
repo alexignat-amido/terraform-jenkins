5dd949773588ed69be0e845cd9524cd9c9205caf
alexignat-amido <alex.ignat@amido.com>
2019-02-20 09:57:56 +0000

    changing config in getenvironment script 2

--- scripts/getenvironment.sh
+++ scripts/getenvironment.sh
@@ -7,6 +7,6 @@ set -e
 # FOO and BAZ shell variables.
 # jq will ensure that the values are properly quoted
 # and escaped for consumption by the shell.
-eval "$(jq -r3424 '@sh "WORKSPACE=\(.workspace) PROJECTCODE=\(.projectcode) URL=\(.url)"')"
+eval "$(jq -r '@sh "WORKSPACE=\r(.workspace) PROJECTCODE=\r(.projectcode) URL=\r(.url)"')"
 # Placeholder for whatever data-fetching logic your script implements
 curl --header "querytext: $WORKSPACE-$PROJECTCODE" $URL
